4f12cca763bbb3c22f0197aec2014eac07654415
Ruben54213 <rubentest193@gmail.com>
2024-10-31 23:43:01 +0100

    Config Logic and Pom.xml change.

--- src/main/java/de/ruben/simplecore/SimpleCore.java
+++ src/main/java/de/ruben/simplecore/SimpleCore.java
@@ -6,12 +6,24 @@ public final class SimpleCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // Konfigurationsdatei erstellen, falls sie noch nicht existiert
+        saveDefaultConfig();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    // Methode zum Neuladen der Konfiguration
+    //public void reloadPluginConfig() {
+    //    reloadConfig();
+    //    getLogger().info("Config.yml wurde neu geladen.");
+    //}
+
+    // Beispielmethode, um einen Wert aus der Config zu holen
+    // public String getExampleSetting() {
+    //    return getConfig().getString("example-setting");
+    // }
+
 }
