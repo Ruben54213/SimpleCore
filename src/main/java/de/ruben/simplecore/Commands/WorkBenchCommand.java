942b1ae6024e9a972ffe1a4196f329d25f3851ad
Ruben54213 <rubentest193@gmail.com>
2024-11-01 00:54:38 +0100

    Updated the workbench module.

--- src/main/java/de/ruben/simplecore/Commands/WorkBenchCommand.java
+++ src/main/java/de/ruben/simplecore/Commands/WorkBenchCommand.java
@@ -4,16 +4,20 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WorkBenchCommand implements CommandExecutor {
 
     private final JavaPlugin plugin;
+    private final FileConfiguration config;
 
     public WorkBenchCommand(JavaPlugin plugin) {
         this.plugin = plugin;
+        this.config = plugin.getConfig();
         this.plugin.getCommand("workbench").setExecutor(this);
+        setupDefaultConfig();
     }
 
     @Override
@@ -30,6 +34,26 @@ public class WorkBenchCommand implements CommandExecutor {
     }
 
     private String getMessage(String path) {
-        return ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString(path, "&7Es ist ein &cFehler&7 aufgetreten, bitte melde dich im &eSupport&7. &cGesuchter Path&7: " + path));
+        return ChatColor.translateAlternateColorCodes('&', config.getString(path, "&7Es ist ein &cFehler&7 aufgetreten, bitte melde dich im &eSupport&7. &cGesuchter Path&7: " + path));
+    }
+
+    // Methode zum Überprüfen und Hinzufügen von Standardwerten in der Konfiguration
+    private void setupDefaultConfig() {
+        if (!config.contains("language")) {
+            config.set("language", "en");
+        }
+        if (!config.contains("messages.en.only-players")) {
+            config.set("messages.en.only-players", "Only players can execute this command!");
+        }
+        if (!config.contains("messages.en.workbench-open")) {
+            config.set("messages.en.workbench-open", "&7The &eworkbench&7 has been &asuccessfully&7 opened!");
+        }
+        if (!config.contains("messages.de.only-players")) {
+            config.set("messages.de.only-players", "Diesen Befehl dürfen nur Spieler ausführen!");
+        }
+        if (!config.contains("messages.de.workbench-open")) {
+            config.set("messages.de.workbench-open", "&7Die &eWerkbank&7 wurde &aErfolgreich&7 geöffnet!");
+        }
+        plugin.saveConfig(); // Konfigurationsdatei speichern, falls neue Werte hinzugefügt wurden
     }
 }
